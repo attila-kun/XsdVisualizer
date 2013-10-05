08f8a8f90e39193fc51ed368ab6b7942d6fec8e3
Attila Kun <kun.attila.hun@gmail.com>
2013-10-05 19:22:21 +0200

    TypeScript compilation is configured in Web project file, basic controllers and views are added.

---
+++ XsdVisualizer.Web/Controllers/XsdVisualizerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XsdVisualizer.Web.Controllers
+{
+    public class XsdVisualizerController : Controller
+    {
+        //
+        // GET: /XsdVisualizer/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
