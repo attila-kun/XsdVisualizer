74fc68f6aac645e758e29bc1e1eb1234fddb6d0b
Attila Kun <kun.attila.hun@gmail.com>
2013-10-09 20:58:32 +0200

    Continued declaring XSD document model.

--- XsdVisualizer.Web/Scripts/XsdModel.ts
+++ XsdVisualizer.Web/Scripts/XsdModel.ts
@@ -8,16 +8,28 @@ module XsdVisualizer.Model {
 		Name: string;
 	}
 
+	interface BuiltInType extends Type {
+
+	}
+
 	interface SimpleType extends Type {
 
 	}
 
 	interface ComplexType extends Type {
+		Sequence: Sequence;
+	}
+
+	interface Sequence {
 		Elements: Element[];
 	}
 
 	interface Element {
 		Name: string;
 		Type: Type;
+	}
+
+	interface Restriction {
+		Base: Type
 	}
 }
