86d1959437fedbcfd7c708dad428ff74dbf1144a
Attila Kun <kun.attila.hun@gmail.com>
2013-10-17 11:54:43 +0200

    Generated .js files are removed from solution.

--- XsdVisualizer.Web/Scripts/XsdVisualizer.js
+++
@@ -1,9 +0,0 @@
-///<reference path="References.ts" />
-var XsdVisualizer;
-(function (XsdVisualizer) {
-    function visualize(markup) {
-        var document = XsdVisualizer.Parser.parse(markup);
-        return new XsdVisualizer.Drawing.VisualizerView(document);
-    }
-    XsdVisualizer.visualize = visualize;
-})(XsdVisualizer || (XsdVisualizer = {}));
