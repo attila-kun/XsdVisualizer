05a0bd28300b3ecbddda36388bcbc03ba29f40ca
Attila Kun <kun.attila.hun@gmail.com>
2013-10-17 12:56:38 +0200

    Scripts folder structure reorganized to avoid tracking TypeScript generated .js files in git.

--- XsdVisualizer.Web/Scripts/TypeScript/References.ts
+++ XsdVisualizer.Web/Scripts/TypeScript/References.ts
@@ -1,10 +1,10 @@
 //Collecting type definition references in a single file, so we will only have to maintain them at a single place.
 
-///<reference path="Thirdparty/jquery.d.ts" />
-///<reference path="Thirdparty/raphael.d.ts" />
+///<reference path="../Thirdparty/jquery.d.ts" />
+///<reference path="../Thirdparty/raphael.d.ts" />
+///<reference path="../Thirdparty/raphael.group.d.ts" />
 ///<reference path="Model.ts" />
 ///<reference path="Parser.ts" />
 ///<reference path="Drawing/VisualizerView.ts" />
 ///<reference path="Drawing/DocumentView.ts" />
 ///<reference path="Drawing/ElementView.ts" />
-///<reference path="Drawing/raphael.group.d.ts" />
